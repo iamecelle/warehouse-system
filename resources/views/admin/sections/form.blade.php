9026d24b3b91d58b07f21afa19b0889fcc36bb36
Mecel <40450634+iamecelle@users.noreply.github.com>
2019-02-13 17:47:35 +0800

    Update form.blade.php

--- resources/views/admin/sections/form.blade.php
+++ resources/views/admin/sections/form.blade.php
@@ -1,18 +1,18 @@
-<div class="form-group {{ $errors->has('course_code') ? 'has-error' : ''}}">
-    {!! Form::label('course_code', 'Course Code', ['class' => 'control-label']) !!}
-    {!! Form::text('course_code', null, ('required' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!}
-    {!! $errors->first('course_code', '<p class="help-block">:message</p>') !!}
+<div class="form-group {{ $errors->has('section_code') ? 'has-error' : ''}}">
+    {!! Form::label('name', 'Section', ['class' => 'control-label']) !!}
+    {!! Form::text('name', null, ('required' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!}
+    {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
 </div>
-<div class="form-group {{ $errors->has('course_description') ? 'has-error' : ''}}">
-    {!! Form::label('course_description', 'Course Description', ['class' => 'control-label']) !!}
-    {!! Form::text('course_description', null, ('required' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!}
-    {!! $errors->first('course_description', '<p class="help-block">:message</p>') !!}
+<div class="form-group {{ $errors->has('section_description') ? 'has-error' : ''}}">
+    {!! Form::label('description', 'Description', ['class' => 'control-label']) !!}
+    {!! Form::text('description', null, ('required' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!}
+    {!! $errors->first('description', '<p class="help-block">:message</p>') !!}
 </div>
-<div class="form-group {{ $errors->has('department_id') ? 'has-error' : ''}}">
-    {!! Form::label('department_id', 'Department Id', ['class' => 'control-label']) !!}
-    {{-- {!! Form::number('department_id', null, ('' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!} --}}
-    {!! Form::select('department_id', $departments, null, ['class' => 'form-control']) !!}
-    {!! $errors->first('department_id', '<p class="help-block">:message</p>') !!}
+<div class="form-group {{ $errors->has('location_id') ? 'has-error' : ''}}">
+    {!! Form::label('location_id', 'Location Id', ['class' => 'control-label']) !!}
+    {{-- {!! Form::number('location_id', null, ('' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!} --}}
+    {!! Form::select('location_id', $locations, null, ['class' => 'form-control']) !!}
+    {!! $errors->first('location_id', '<p class="help-block">:message</p>') !!}
 </div>
 
 
