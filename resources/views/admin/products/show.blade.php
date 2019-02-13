862c7e63e59c321a21eb24011e2bb60a8464bfcf
Mecel <40450634+iamecelle@users.noreply.github.com>
2019-02-13 17:44:16 +0800

    Update show.blade.php

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -7,20 +7,19 @@
 
             <div class="col-md-9 col-md-offset-1">
                 <div class="box">
-                    <div class="box-header">Student <b>{{ $student->full_name }}</b></div>
-                    <div class="box-body">
+                    <div class="box-header">Product <b>{{ $product->name }}</b></div> <div class="box-body">
 
-                        <a href="{{ url('/admin/students') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-                        <a href="{{ url('/admin/students/' . $student->id . '/edit') }}" title="Edit Student"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                        <a href="{{ url('/admin/products') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
+                        <a href="{{ url('/admin/products/' . $product->id . '/edit') }}" title="Edit Product"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
                         {!! Form::open([
                             'method'=>'DELETE',
-                            'url' => ['admin/students', $student->id],
+                            'url' => ['admin/products', $product->id],
                             'style' => 'display:inline'
                         ]) !!}
                             {!! Form::button('<i class="fa fa-trash-o" aria-hidden="true"></i> Delete', array(
                                     'type' => 'submit',
                                     'class' => 'btn btn-danger btn-sm',
-                                    'title' => 'Delete Student',
+                                    'title' => 'Delete Product',
                                     'onclick'=>'return confirm("Confirm delete?")'
                             ))!!}
                         {!! Form::close() !!}
@@ -31,9 +30,9 @@
                             <table class="table table-borderless">
                                 <tbody>
                                     <tr>
-                                        <th>ID</th><td>{{ $student->id }}</td>
+                                        <th>ID</th><td>{{ $product->id }}</td>
                                     </tr>
-                                    <tr><th> Id Number </th><td> {{ $student->id_number }} </td></tr><tr><th> First Name </th><td> {{ $student->first_name }} </td></tr><tr><th> Last Name </th><td> {{ $student->last_name }} </td></tr>
+                                    <tr><th> Name</th><td> {{ $product->name }} </td></tr><tr><th> Description </th><td> {{ $product->description }} </td></tr><tr><th>Section ID </th><td> {{ $product->section_id }} </td></tr>
                                 </tbody>
                             </table>
                         </div>
