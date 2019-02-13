af2979a881f669300c9a41d646f3bf2b13e15fe6
Mecel <40450634+iamecelle@users.noreply.github.com>
2019-02-13 17:45:07 +0800

    Update create.blade.php

--- resources/views/admin/sections/create.blade.php
+++ resources/views/admin/sections/create.blade.php
@@ -7,9 +7,9 @@
 
             <div class="col-md-9">
                 <div class="card">
-                    <div class="card-header">Create New Course</div>
+                    <div class="card-header">Create New Section</div>
                     <div class="card-body">
-                        <a href="{{ url('/admin/courses') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
+                        <a href="{{ url('/admin/sections') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
                         <br />
                         <br />
 
@@ -21,9 +21,9 @@
                             </ul>
                         @endif
 
-                        {!! Form::open(['url' => '/admin/courses', 'class' => 'form-horizontal', 'files' => true]) !!}
+                        {!! Form::open(['url' => '/admin/sections', 'class' => 'form-horizontal', 'files' => true]) !!}
 
-                        @include ('admin.courses.form', ['formMode' => 'create'])
+                        @include ('admin.sections.form', ['formMode' => 'create'])
 
                         {!! Form::close() !!}
 
