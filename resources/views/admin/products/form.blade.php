97a4b8ccd557bc2e4ef10fa12a2d134034d4560b
Mecel <40450634+iamecelle@users.noreply.github.com>
2019-02-13 17:41:11 +0800

    Update form.blade.php

--- resources/views/admin/products/form.blade.php
+++ resources/views/admin/products/form.blade.php
@@ -1,27 +1,17 @@
-<div class="form-group {{ $errors->has('id_number') ? 'has-error' : ''}}">
-    {!! Form::label('id_number', 'Id Number', ['class' => 'control-label']) !!}
-    {!! Form::text('id_number', null, ('required' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!}
-    {!! $errors->first('id_number', '<p class="help-block">:message</p>') !!}
+<div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
+    {!! Form::label('name', 'Name', ['class' => 'control-label']) !!}
+    {!! Form::text('name', null, ('required' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!}
+    {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
 </div>
-<div class="form-group {{ $errors->has('first_name') ? 'has-error' : ''}}">
-    {!! Form::label('first_name', 'First Name', ['class' => 'control-label']) !!}
-    {!! Form::text('first_name', null, ('required' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!}
-    {!! $errors->first('first_name', '<p class="help-block">:message</p>') !!}
+<div class="form-group {{ $errors->has('description') ? 'has-error' : ''}}">
+    {!! Form::label('description', 'Description', ['class' => 'control-label']) !!}
+    {!! Form::text('description', null, ('required' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!}
+    {!! $errors->first('description', '<p class="help-block">:message</p>') !!}
 </div>
-<div class="form-group {{ $errors->has('last_name') ? 'has-error' : ''}}">
-    {!! Form::label('last_name', 'Last Name', ['class' => 'control-label']) !!}
-    {!! Form::text('last_name', null, ('required' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!}
-    {!! $errors->first('last_name', '<p class="help-block">:message</p>') !!}
-</div>
-<div class="form-group {{ $errors->has('gender') ? 'has-error' : ''}}">
-    {!! Form::label('gender', 'Gender', ['class' => 'control-label']) !!}
-    {!! Form::select('gender', json_decode('{"male":"Male","female":"Female"}', true), null, ('' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!}
-    {!! $errors->first('gender', '<p class="help-block">:message</p>') !!}
-</div>
-<div class="form-group {{ $errors->has('course_id') ? 'has-error' : ''}}">
-    {!! Form::label('course_id', 'Course', ['class' => 'control-label']) !!}
-    {!! Form::select('course_id', $courses, null, ['class' => 'form-control', 'id' => 'courses']) !!}
-    {!! $errors->first('course_id', '<p class="help-block">:message</p>') !!}
+<div class="form-group {{ $errors->has('section_id') ? 'has-error' : ''}}">
+    {!! Form::label('section_id', 'Section', ['class' => 'control-label']) !!}
+    {!! Form::select('section_id', $sections, null, ['class' => 'form-control', 'id' => 'sections']) !!}
+    {!! $errors->first('section_id', '<p class="help-block">:message</p>') !!}
 </div>
 
 
