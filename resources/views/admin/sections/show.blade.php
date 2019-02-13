397d935905a4564d2b3a10aa56807e69f6670aa8
Mecel <40450634+iamecelle@users.noreply.github.com>
2019-02-13 17:48:18 +0800

    Update show.blade.php

--- resources/views/admin/sections/show.blade.php
+++ resources/views/admin/sections/show.blade.php
@@ -7,20 +7,20 @@
 
             <div class="col-md-9">
                 <div class="card">
-                    <div class="card-header">Course {{ $course->id }}</div>
+                    <div class="card-header">Section {{ $section->id }}</div>
                     <div class="card-body">
 
-                        <a href="{{ url('/admin/courses') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-                        <a href="{{ url('/admin/courses/' . $course->id . '/edit') }}" title="Edit Course"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                        <a href="{{ url('/admin/sections') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
+                        <a href="{{ url('/admin/sections/' . $section->id . '/edit') }}" title="Edit Section"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
                         {!! Form::open([
                             'method'=>'DELETE',
-                            'url' => ['admin/courses', $course->id],
+                            'url' => ['admin/sections', $section->id],
                             'style' => 'display:inline'
                         ]) !!}
                             {!! Form::button('<i class="fa fa-trash-o" aria-hidden="true"></i> Delete', array(
                                     'type' => 'submit',
                                     'class' => 'btn btn-danger btn-sm',
-                                    'title' => 'Delete Course',
+                                    'title' => 'Delete Section',
                                     'onclick'=>'return confirm("Confirm delete?")'
                             ))!!}
                         {!! Form::close() !!}
@@ -31,9 +31,9 @@
                             <table class="table table-borderless">
                                 <tbody>
                                     <tr>
-                                        <th>ID</th><td>{{ $course->id }}</td>
+                                        <th>ID</th><td>{{ $section->id }}</td>
                                     </tr>
-                                    <tr><th> Course Code </th><td> {{ $course->course_code }} </td></tr><tr><th> Course Description </th><td> {{ $course->course_description }} </td></tr><tr><th> Department Id </th><td> {{ $course->department_id }} </td></tr>
+                                    <tr><th> Section Code </th><td> {{ $section->name }} </td></tr><tr><th> Section Description </th><td> {{ $section->description }} </td></tr><tr><th> Location Id </th><td> {{ $section->location_id }} </td></tr>
                                 </tbody>
                             </table>
                         </div>
@@ -43,16 +43,14 @@
                 <table class="table table-responsive">
                     <thead>
                         <tr>
-                            <th>Course Code</th>
-                            <th>Student ID</th>
-                            <th>Name</th>
+                            <th>Section</th>
+                            <th>Description</th>
                         </tr>
                     </thead>
-                    @foreach ($students as $student)
+                    @foreach ($products as $product)
                         <tr>
-                            <td>{{ $student->course->course_code }}</td>
-                            <td>{{ $student->id_number }}</td>
-                            <td>{{ $student->last_name.', '.$student->first_name }}</td>
+                            <td>{{ $product->name }}</td>
+                            <td>{{ $product->description }}</td>
                         </tr>
                     @endforeach
                 </table>
